6d02aff037b824ba64b7e183adcff85da5536828
Vitalik-Litvinchuk <61794076+Vitalik-Litvinchuk@users.noreply.github.com>
2024-11-03 12:15:08 +0200

    feat: update data

--- tests/Tests.Data/AssignmentsData.cs
+++ tests/Tests.Data/AssignmentsData.cs
@@ -5,6 +5,6 @@ namespace Tests.Data;
 
 public static class AssignmentsData
 {
-    public static Assignment MainAssignment(AssignmentId assignmentId, CourseId courseId)
-        => Assignment.New(assignmentId, courseId, "Main assignment", "Main assignment description", DateTime.UtcNow.AddDays(7), 100m, DateTime.UtcNow);
+    public static Assignment MainAssignment(CourseId courseId)
+        => Assignment.New(AssignmentId.New(), courseId, "Main assignment", "Main assignment description", DateTime.UtcNow.AddDays(7), 100m, DateTime.UtcNow);
 }
